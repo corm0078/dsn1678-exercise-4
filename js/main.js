b2ec7d8be3d07ed9ad10f8b787c4bc3fa2f52f0c
corm0078 <corm0078@algonquinlive.com>
2014-10-07 10:14:12 -0400

    playing with shuttle

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 // $reminds you that is a jquery object
-var $win=$(window);
-var $sun=$(".sun");
+var $win = $(window);
+var $sun = $(".sun");
+var $sunSection = $(".sun-section");
+var $dipperSection = $(".dipper-section");
+var $dipper = $(".dipper");
+var $shuttleSection = $(".shuttle-section");
+var $shuttle = $(".shuttle");
 
-$win.on("scroll", function(){
+$win.on("scroll", function () {
     var scrollPos= $win.scrollTop();
     
-$sun.css("transform","rotate(" +scrollPos/5+ "deg)");   
+$sun.css("transform", "rotate(" + scrollPos/5 + "deg)"); 
+$sunSection.css("background-position", "center " + scrollPos/2 + "px")    
+});
+
+$dipperSection.waypoint(function () {
+  $dipper.addClass("js-dipper-fade");
+}, { offset: "50%" });
+
+$shuttleSection.waypoint(function () {
+  $shuttle.addClass("js-shuttle-fade");
+}, { offset: "75%" });
+
+    
+$shuttle.css("transform", "margin-top(" + scrollPos/5 + "-4)"); 
+$shuttleSection.css("background-position", "center " + scrollPos/2 + "px")    
 });
